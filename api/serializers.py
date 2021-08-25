2e6940af5e450eab1120b2f04d3d1274c97bc698
mailzahidul <mailzahidul@gmail.com>
2021-08-25 23:43:53 +0600

    object label validation

--- api/serializers.py
+++ api/serializers.py
@@ -27,9 +27,26 @@ class StudentModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id','name', 'roll', 'city']
-    
+
+# Field Lavel validation   
+
     def validate_roll(self, value):
         if value >= 200:
             raise serializers.ValidationError('Set Full')
         return value
+    
+    def validate_name(self, value):
+        if value[0].lower() != 'r':
+            raise serializers.ValidationError('Name is not started with R')
+        return value
+
+
+# object lavel validation error
+    def validate(self, data):
+        nm = data.get('name')
+        ct= data.get('city')
+        if len(nm) >= 10 and len(ct) >= 10:
+            raise serializers.ValidationError('Its too large')
+        return data
+
     
